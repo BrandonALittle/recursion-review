34bae17801d9585f845e976791386a31a10462a9
BrandonALittle <brandon.a.little@gmail.com>
2018-01-17 19:40:22 -0800

    Implemented non-functioning stringifyJSON().... still working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // undefined
+  // null
+  // string
+  // boolean
+  // number
+  // array
+  // obj
+  if (obj === undefined) {
+    return undefined;
+  } else if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'boolean') {
+    return obj.toString();
+  } else if (typeof obj === 'number') {
+    return obj.toString();
+  } else if (Array.isArray(obj)) {
+    let result = [];
+    obj.forEach(function(elem) {
+      if (elem === undefined) {
+        return result.concat[0];
+      } else {
+        return '[' + result.concat(stringifyJSON(elem)).join(',') + ']';
+      }
+    });
+  } else if (obj.constructor === 'Object') {
+    let keys = Object.keys(obj);
+    let result = [];
+    for (let key in keys) {
+      if (obj[key] === undefined) {
+        return result;
+      } else {
+        result.push('"' + key + '":'); // 'key:'
+        result.push(stringifyJSON(obj));
+      }
+    }
+    result = '{' + result.join(',') + '}';
+  }
 };
